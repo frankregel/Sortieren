4e736b72fce53d38b536816082b5d1d9b257c0c8
frankregel <frank.regel@gmail.com>
2014-02-06 14:25:56 +0100

    Initial Commit

---
+++ Sortieren/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sortieren
+//
+//  Created by Frank on 06.02.14.
+//  Copyright (c) 2014 Frank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
